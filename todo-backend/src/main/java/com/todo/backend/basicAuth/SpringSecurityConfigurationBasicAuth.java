2e9caa1c4ec2f70913acfa2179f7f94db3a4dce2
Lipi727 <lipiprajapati727@gmail.com>
2021-08-01 19:00:34 +0530

    added basicAuthentication

--- todo-backend/src/main/java/com/todo/backend/basicAuth/SpringSecurityConfigurationBasicAuth.java
+++ todo-backend/src/main/java/com/todo/backend/basicAuth/SpringSecurityConfigurationBasicAuth.java
@@ -1,21 +1,23 @@
-//package com.todo.backend.basicAuth;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter{
-////	@Override
-////	protected void configure(HttpSecurity http) throws Exception {
-////		http
-////		.csrf().disable()	
-////		.authorizeRequests()
-////		.antMatchers(HttpMethod.GET,"/**").permitAll()
-////				.anyRequest().authenticated()
-////				.and()
-////			//.formLogin().and()
-////			.httpBasic();
-////	}
-//}
+package com.todo.backend.basicAuth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter{
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.csrf().disable()	
+		.authorizeRequests()
+		.antMatchers(HttpMethod.OPTIONS,"/**").permitAll() //to authenticate all the url
+				.anyRequest().authenticated()
+				.and()
+			//.formLogin().and()
+			.httpBasic();
+	}
+}
--- todo-frontend/src/app/components/services/todo.service.ts
+++ todo-frontend/src/app/components/services/todo.service.ts
@@ -1,12 +1,33 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class TodoService {
+  getAllTodo() {
+    let basicAuthHeaderString = this.createBasicAuthenticationHttpHeader();
 
-  getAllTodo(){
-    return this.http.get('http://localhost:8080/users/user/todos');
+    let headers = new HttpHeaders({
+      Authorization: basicAuthHeaderString,
+    });
+
+    return this.http.get('http://localhost:9090/users/user/todos', {headers});
   }
 
-  constructor(private http:HttpClient) { }
+  // Access to XMLHttpRequest at 'http://localhost:9090/users/user/todos'
+  // from origin 'http://localhost:4200' has been blocked by CORS policy: No
+  //  'Access-Control-Allow-Origin'
+  // header is present on the requested resource.
+
+  // Access to XMLHttpRequest at 'http://localhost:9090/users/user/todos' from origin 
+  // 'http://localhost:4200' has been blocked by CORS policy: Response to preflight request doesn't 
+  // pass access control check: It does not have HTTP ok status.
+  createBasicAuthenticationHttpHeader() {
+    let username = 'Lipi'
+    let password = '12345'
+    let basicAuthHeaderString =
+      'Basic ' + window.btoa(username + ':' + password)
+    return basicAuthHeaderString;
+  }
+
+  constructor(private http: HttpClient) {}
 }
